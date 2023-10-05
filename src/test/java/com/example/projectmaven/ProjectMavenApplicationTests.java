7041c9d6329352e86e3a5a619e8d54a090719a65
Crazywolf <vaclav.vlcek@seznam.cz>
2023-10-05 11:27:39 +0200

    test)

---
+++ src/test/java/com/example/projectmaven/ProjectMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
