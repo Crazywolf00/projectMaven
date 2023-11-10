0884d7206868fb544e5189247d586eb65d42d259
Crazywolf <vaclav.vlcek@seznam.cz>
2023-11-10 15:31:16 +0100

    add style and more button to mine referral

--- react-deniska/src/user/MineReferral.js
+++ react-deniska/src/user/MineReferral.js
@@ -1,16 +1,36 @@
 import './MineReferral.css'
+
 function MineReferral({setShowReferral}) {
 
     function returnMain() {
         setShowReferral(true)
     }
 
+    function goToGithub() {
+        window.open('https://github.com/Crazywolf00', '_blank')
+    }
+
+    function goToLinkedin() {
+        window.open('https://www.linkedin.com/in/václav-vlček-96924b287', '_blank')
+    }
+
+
     return (
-        <div id={'mine-referral'}>
-            <div className={'mine-ref'} id={'git-hub'}></div>
-            <div className={'mine-ref'} id={'return'} onClick={returnMain}></div>
-            <div className={'mine-ref'} id={'linkedin'}></div>
+        <div>
+            <div id={'mine-referral'}>
+                <div className={'mine-ref'} id={'git-hub'} onClick={goToGithub}></div>
+                <div className={'mine-ref'} id={'return'} onClick={returnMain}></div>
+                <div className={'mine-ref'} id={'linkedin'} onClick={goToLinkedin}></div>
+            </div>
+            <div id={'info-referral'}>
+                <div id={'more-info-referral'}>
+                    <div>SEE MORE</div>
+                    <div> ▼</div>
+                </div>
+                <div>info</div>
+            </div>
         </div>
     )
 }
+
 export default MineReferral
