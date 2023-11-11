c3806d8d02839d4815c06cf8af724af3f574130a
Crazywolf <vaclav.vlcek@seznam.cz>
2023-11-11 11:50:38 +0100

    add call for data and style

--- react-deniska/src/user/UserContentMain.js
+++ react-deniska/src/user/UserContentMain.js
@@ -1,21 +1,53 @@
 import './UserContentMain.css'
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import axios from "../axios";
+import {SERVER_URL} from "../config";
 
 function UserContentMain({category}) {
 
-    useEffect(() => {
+    const [message, setMessage] = useState("");
+    const [content, setContent] = useState([]);
 
-        console.log(category);
+    useEffect(() => {
+        setMessage("")
+        setContent([])
+        if (category.length > 0) {
+            axios.get(`/api/content?category=${category}`)
+                .then(response => response.data)
+                .then(data => {
+                    setMessage(data.welcomeMessage.message)
+                    setContent(data.sorteImageList)
+                })
+        } else {
+
+        }
+
     }, [category]);
 
     return (
         <div>
             {category === "" ? <div></div> : (
                 <div id="user-content-main">
-                    {category}
+                    <div id={'user-category-topic'}>{category}</div>
+                    {message && <div id={'user-message-welcome'}>{message}</div>}
+                    {content && content.map((oneContent, contentIndex) => (
+                        <div key={contentIndex} id={'user-content-welcome'}>
+                            <div>{oneContent.setName}</div>
+                            {oneContent.images.map((img, imgIndex) => (
+                                <div style={{
+                                    display: 'inline-flex'
+                                }}>
+                                    <div key={imgIndex} className={'user-img-show'} style={{
+                                        backgroundImage: `url(${SERVER_URL}/api/getImg/${img.id})`,
+                                    }}></div>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
                 </div>
             )}
         </div>
     );
 }
+
 export default UserContentMain
